ebe6a557219ae3d34b56bd72e53e139bebabdb16
Yulia Burenko <juliaburenko@yandex.ru>
2023-01-29 13:32:38 +0300

    initial commit closes #1

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
