29c6055b4a13efca7ffe5c2be5c82c885c16ca9a
Yulia Burenko <juliaburenko@yandex.ru>
2023-01-30 18:19:16 +0300

    feat: cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
@@ -13,6 +14,16 @@ const errorHandler = require('./middlewares/errorHandler');
 const { PORT = 3000 } = process.env;
 const { DATABASE__URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 const app = express();
+
+app.use(cors({
+  origin: ['https://julbrndiploma.nomoredomainsclub.ru', 'http://julbrndiploma.nomoredomainsclub.ru', 'https://api.julbrndiploma.nomoredomainsclub.ru', 'http://api.julbrndiploma.nomoredomainsclub.ru', 'http://localhost:3000',
+    'https://localhost:3000',
+    'http://localhost:3001',
+    'https://localhost:3001',
+    'localhost:3000',
+  ],
+  credentials: true,
+}));
 
 mongoose.connect(DATABASE__URL, {
   useNewUrlParser: true,
